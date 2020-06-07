7fb5c574843ce8d16ba6f5755ef3a16e2ec5b2f3
olympia <owojcik7@gmail.com>
2020-06-07 13:00:30 -0400

    Implement Output Users using forEach() & DOM Methods

--- script.js
+++ script.js
@@ -24,10 +24,33 @@ async function getRandomUser() {
 
     }
 
-    addData(console.log(newUser))
+    addData(newUser)
 }
 
 // Add new object to data array
 function addData(obj) {
     data.push(obj);
+
+    updateDOM()
 }
+
+// Update DOM
+// If nothing is passed in, use data array
+function updateDOM(providedData = data) {
+    // Clear main div
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>'
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person')
+        element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`;
+        main.appendChild(element)
+    })
+}
+
+// Format number as money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser)
